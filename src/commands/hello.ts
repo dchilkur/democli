e994f597af41d8540fb49a0ed9426e9b188d5db8
Dinesh Chilkuri <dchilkuri@salesforce.com>
2020-06-03 07:07:56 -0700

    demo code for args and flags

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -15,15 +15,25 @@ hello world from ./src/hello.ts!
     name: flags.string({char: 'n', description: 'name to print'}),
     // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
+    // flag with a value (-l, --language=VALUE), is required? required:true
+    language: flags.string({char: 'l', description: 'language to greet in', default: 'spanish'}),
   }
 
   static args = [{name: 'file'}]
 
+  // static strict=false
+
   async run() {
     const {args, flags} = this.parse(Hello)
+    this.log(args)
 
     const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
+    this.log(`${flags.language}`)
+    if (flags.language === 'english') {
+      this.log(`hello ${name} from ./src/commands/hello.ts`)
+    } else {
+      this.log(`hola ${name} from ./src/commands/hello.ts`)
+    }
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
     }
